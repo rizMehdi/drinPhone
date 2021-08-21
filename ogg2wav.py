a0fb784eb39262b7a2175ad1fb587210b3d68ec0
rizMehdi <ccmeall@gmail.com>
2021-08-21 17:05:37 +0200

    Update ogg2wav.py

--- ogg2wav.py
+++ ogg2wav.py
@@ -1,1 +1,7 @@
-
+#import IPython.display as ipd
+from pydub import AudioSegment
+input_audiofile="audio.ogg"
+output_audiofile="audio.mp3"
+loadaudio = AudioSegment.from_ogg(input_audiofile)
+loadaudio.export(output_audiofile, format="mp3")
+#ipd.display(ipd.Audio("sampleMp3.mp3", autoplay=False))
